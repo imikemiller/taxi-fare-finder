dd33b29883825a3250b710b1da3c1b0bf78c99e4
Mike Miller <mike@mikemiller.uk>
2018-06-30 13:21:37 +0200

    Added eventful.php config

---
+++ config/eventful.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mike
+ * Date: 30/06/18
+ * Time: 13:20
+ */
+return [
+    'api_key'=>env('EVENTFUL_API_KEY'),
+    'base_url'=>env('EVENTFUL_BASE_URL')
+];
